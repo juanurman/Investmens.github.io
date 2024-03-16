6e87d11e0491fe9336a1cd43d2b8cc621aeee4c8
juanurman <juanurman@gmail.com>
2024-03-16 18:48:02 -0300

    seguimos

---
+++ routes/info.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('pages/acciones/info', { title: 'INFO' });
+});
+router.get('/appl', function(req, res, next) {
+  res.render('pages/acciones/appl', { title: 'INFO' });
+});
+
+router.get('/amzn', function(req, res, next) {
+  res.render('pages/acciones/amzn', { title: 'INFO' });
+});
+
+router.get('/googl', function(req, res, next) {
+  res.render('pages/acciones/googl', { title: 'INFO' });
+});
+
+router.get('/googl', function(req, res, next) {
+  res.render('pages/acciones/googl', { title: 'INFO' });
+});
+
+module.exports = router;
