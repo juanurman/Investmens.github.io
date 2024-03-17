b38c713364f9c3c690b48af2d6c46259cd6e0ed0
juanurman <juanurman@gmail.com>
2024-03-17 19:23:27 -0300

    un par mas

--- routes/info.js
+++ routes/info.js
@@ -17,8 +17,11 @@ router.get('/googl', function(req, res, next) {
   res.render('pages/acciones/googl', { title: 'INFO' });
 });
 
-router.get('/googl', function(req, res, next) {
-  res.render('pages/acciones/googl', { title: 'INFO' });
+router.get('/msft', function(req, res, next) {
+  res.render('pages/acciones/msft', { title: 'INFO' });
+});
+router.get('/ko', function(req, res, next) {
+  res.render('pages/acciones/ko', { title: 'INFO' });
 });
 
 module.exports = router;
